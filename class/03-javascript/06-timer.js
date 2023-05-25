a6d6072c4f333127762d57106135852705babcab
fe-jiwon <jiwon.jo@seedglobal.co>
2023-05-25 16:54:22 +0900

    study

---
+++ class/03-javascript/06-timer.js
@@ -0,0 +1,31 @@
+let isStarted = false;
+
+const auth = () => {
+  if (isStarted === false) {
+    // 타이머가 작동중이 아닐때
+    isStarted = true;
+    document.getElementById("finish").disabled = false;
+    const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+    document.getElementById("target").innerText = token;
+    document.getElementById("target").style.color = "#" + token;
+
+    let time = 5;
+    let timer;
+
+    timer = setInterval(function () {
+      if (time >= 0) {
+        const min = Math.floor(time / 60);
+        const sec = String(time % 60).padStart(2, "0");
+        document.getElementById("timer").innerText = min + ":" + sec;
+        time = time - 1;
+      } else {
+        document.getElementById("finish").disabled = true;
+        isStarted = false;
+        console.log("타이머 작동중");
+        clearInterval(timer);
+      }
+    }, 1000);
+  } else {
+    // 타이머가 작동중일때
+  }
+};
---
+++ class/03-javascript/연습-08-timer.js
@@ -0,0 +1,10 @@
+let time = 180;
+
+setInterval(function () {
+  if (time >= 0) {
+    const min = Math.floor(time / 60);
+    const sec = String(time % 60).padStart(2, "0");
+    console.log(min + ":" + sec);
+    time = time - 1;
+  }
+}, 1000);
